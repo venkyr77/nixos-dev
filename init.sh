f82c2483cec4bf7da2ffe335a545e1bdf372e28c
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-09-06 10:33:49 -0700

    fixes

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./modules/disko.nix
-sudo nixos-install --max-jobs 4 --cores 2 --flake .# 
+sudo nixos-install --max-jobs 4 --cores 2 --flake .#dev 
